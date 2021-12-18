404a1ce423d0a0d9d31f4118a8e255e8de274f09
inshalworks <88522625+inshalworks@users.noreply.github.com>
2021-12-18 18:22:37 +0500

    YESSS

--- Scripts/script.js
+++ Scripts/script.js
@@ -1,11 +1,13 @@
 const navBtn = document.querySelector('.menu-icon');
 const navItems = document.querySelectorAll('.nav-link-items');
+const topRow = document.querySelector('.top-row');
 const navToggle = () => {
   const navbar = document.querySelector('.nav-links');
 
   navbar.classList.remove('none');
   navbar.classList.toggle('navbar-closed');
   navBtn.classList.toggle('closed');
+  topRow.classList.toggle('fixed');
 };
 navBtn.addEventListener('click', navToggle);
 navItems.forEach((item) => {
